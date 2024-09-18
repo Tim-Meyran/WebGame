cd6378d6a690db7a4658188c3373be1aa872da6c
Tim-Meyran <timmeyran@gmail.com>
2024-09-18 21:40:48 +0200

    added docs folder

--- scenes/character_body_3d.gd
+++ scenes/character_body_3d.gd
@@ -23,7 +23,9 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("PLAYER_LEFT", "PLAYER_RIGHT", "PLAYER_FORWARD", "PLAYER_BACK")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction := Vector3(input_dir.x, 0, input_dir.y)
+	
+	look_at(position + direction,Vector3.UP) 
 	
 	var currSpeed = SPEED
 	if Input.is_action_pressed("PLAYER_SPRINT") and is_on_floor():
@@ -43,17 +45,17 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 	
 	cameraPivot.position = cameraPivot.position.lerp(position,delta*10)
-	cameraPivot.quaternion = cameraPivot.quaternion.slerp(quaternion,delta*20)
+	#cameraPivot.quaternion = cameraPivot.quaternion.slerp(quaternion,delta*20)
 	
-	var speed = abs(velocity.z)#Vector2(velocity.x,velocity.z).length()
+	var speed = Vector2(velocity.x,velocity.z).length()
 	#print(speed)
 	if animationTree:
 		animationTree.set("parameters/BlendSpace1D/blend_position",speed/4.0)
 
-func _unhandled_input(event):
-	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-		if event is InputEventMouseMotion:
-			rotate_y(-event.relative.x  /1000)
+#func _unhandled_input(event):
+#	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+#		if event is InputEventMouseMotion:
+#			rotate_y(-event.relative.x  /1000)
 	
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
